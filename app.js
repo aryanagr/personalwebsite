e3e1b0ed85d5b6029c4468b94b7de7a7fa43b091
aryan-8947 <smartaryanagrawal987@gmail.com>
2021-09-12 14:10:50 +0530

    some commit

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ require('./db/conn');
 
 app.use(require('./router/auth'));
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static("client/build"))
+
+if ( process.env.NODE_ENV == "production"){
+
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+
 }
 
  const PORT=process.env.PORT||4000
