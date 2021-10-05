a5442be8eb770bf345a91e39ccd827a203632219
aryan-8947 <smartaryanagrawal987@gmail.com>
2021-10-05 17:18:03 +0530

    commit

--- app.js
+++ app.js
@@ -11,19 +11,16 @@ require('./db/conn');
 app.use(require('./router/auth'));
 
 
-// if ( process.env.NODE_ENV == "production"){
-//  app.use(express.static("client/build/index.html"));
-//     const path = require("path");
+if ( process.env.NODE_ENV == "production"){
+ app.use(express.static("client/build/"));
+   
+}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "client/build")));
 //     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     })
-// }
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
+//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
+//   }
 
  const PORT=process.env.PORT || 4000
 
